ad4dd1ec2cc98372c7dcb6ad83f1fc1bc65cfd04
tanaatsu <atsushi19890825@gmail.com>
2021-11-23 21:13:24 +0900

    ユーザー管理機能の実装

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,12 +2,13 @@ FactoryBot.define do
   factory :user do
     nickname               {'test'}
     email                  {'test@yahoo.co.jp'}
-    password               {'0000'}
+    password               {'000000'}
     password_confirmation  {password}
     last_name              {'test'}
     first_name             {'test'}
     last_name_k            {'test'}
     first_name_k           {'test'}
+    birthday               {'1930-01-01'}
     
   end
 end
