45c69d11a1d4590b44f05eadf36ff9cd7c8aaa95
tanaatsu <atsushi19890825@gmail.com>
2021-11-23 21:29:48 +0900

    ユーザー管理機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-
   def create
-    
   end
-
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname               {'test'}
-    email                  {'test@yahoo.co.jp'}
-    password               {'000000'}
-    password_confirmation  {password}
-    last_name              {'test'}
-    first_name             {'test'}
-    last_name_k            {'test'}
-    first_name_k           {'test'}
-    birthday               {'1930-01-01'}
-    
+    nickname               { 'test' }
+    email                  { 'test@yahoo.co.jp' }
+    password               { '000000' }
+    password_confirmation  { password }
+    last_name              { 'test' }
+    first_name             { 'test' }
+    last_name_k            { 'test' }
+    first_name_k           { 'test' }
+    birthday               { '1930-01-01' }
   end
 end
