7c8748a19c2e3f7efaaeba00c07dc4f7ffcb2834
tanaatsu <atsushi19890825@gmail.com>
2021-11-24 10:43:07 +0900

    ユーザー管理機能実装（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_k,        presence: true
-  validates :first_name_k,       presence: true
+  validates :password,                           format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}    
+  validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}    
+  validates :last_name_k,        presence: true, format: {with: /\p{katakana}/}
+  validates :first_name_k,       presence: true, format: {with: /\p{katakana}/}
   validates :birthday,           presence: true
+
 end
