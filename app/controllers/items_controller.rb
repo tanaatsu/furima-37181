625b32dcac6ff1e757ee8508b12dafcacf0e008e
tanaatsu <atsushi19890825@gmail.com>
2021-11-26 12:47:01 +0900

    商品情報編集機能の実装（修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,6 +47,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+   unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to action: :index 
+   end
   end
+  
 end
