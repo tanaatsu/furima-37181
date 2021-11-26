2fcd057537e99c04917d67cd674d92e77941093c
tanaatsu <atsushi19890825@gmail.com>
2021-11-26 15:44:01 +0900

    商品削除機能（修正①）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show,:edit,:update,:destroy,:move_to_index]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, only: [:edit,:destroy]
   
 
   def index
