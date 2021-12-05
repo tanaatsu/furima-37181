8e507c1dd7e88436cf171b89215d4615571018e4
tanaatsu <atsushi19890825@gmail.com>
2021-12-05 15:01:28 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,13 +15,13 @@ class Item < ApplicationRecord
 
   validates  :name,   presence: true
   validates  :text,   presence: true
-  validates :image,   presence: true
+  validates :image,   presence: {message: "を選択してください"}
 
-  validates  :category_id,     numericality: { other_than: 1, message: "can't be blank" }
-  validates  :status_id,       numericality: { other_than: 1, message: "can't be blank" }
-  validates  :postage_id,      numericality: { other_than: 1, message: "can't be blank" }
-  validates  :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
-  validates  :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates  :category_id,     numericality: { other_than: 1, message: "を選択してください" }
+  validates  :status_id,       numericality: { other_than: 1, message: "を選択してください" }
+  validates  :postage_id,      numericality: { other_than: 1, message: "を選択してください" }
+  validates  :prefecture_id,   numericality: { other_than: 1, message: "を選択してください" }
+  validates  :shipping_day_id, numericality: { other_than: 1, message: "を選択してください" }
   validates  :price,           presence: true,
                                numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
                                format: { with: /\A[0-9]+\z/ }
